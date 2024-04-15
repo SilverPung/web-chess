3836c21f8381d0027cc2366e4054f98515fdc6e6
SilverPung <kristofer.pawel@gmail.com>
2024-04-15 19:10:15 +0200

    creting edit site , reparing footer and creting todo

--- chess/chesstour/forms.py
+++ chess/chesstour/forms.py
@@ -9,21 +9,21 @@ from .models import Chess_Tournament, Chess_Player
 class TournamentForm(forms.ModelForm):
     class Meta:
         model = Chess_Tournament
-        fields = ['name', 'description', 'rounds']
+        fields = ['name', 'description']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}),
-            'description': forms.Textarea(attrs={'class': 'form-control rounded-xl bg-gray-300'}),
-            'rounds': forms.NumberInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}),
+            'name': forms.TextInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300 py-2 px-2'}),
+            'description': forms.Textarea(attrs={'class': 'form-control rounded-xl bg-gray-300 py-2 px-2'}),
+
         }
 
 class PlayerForm(forms.ModelForm):
+    tournament = forms.ModelChoiceField(required=False,queryset=Chess_Tournament.objects.all(), widget=forms.HiddenInput())
+    username = forms.CharField(required=False,widget=forms.TextInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}))
+    birth_date = forms.DateField(required=False,widget=forms.TextInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}))
     class Meta:
         model = Chess_Player
-        fields = ['name', 'surname', 'username', 'rating', 'birth_date']
+        fields = ['name', 'surname', 'username', 'birth_date']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}),
             'surname': forms.TextInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}),
-            'username': forms.TextInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}),
-            'rating': forms.NumberInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}),
-            'birth_date': forms.DateInput(attrs={'class': 'form-control rounded-xl py-3 w-full bg-gray-300'}),
         }
--- chess/chesstour/views.py
+++ chess/chesstour/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import TournamentForm
+from .forms import TournamentForm, PlayerForm
 from .models import Chess_Tournament,Chess_Game,Chess_Player
 # Create your views here.
 @login_required
@@ -17,9 +17,36 @@ def create_new(request):
     return render(request, 'chesstour/create_new.html', {'form': form})
 
 @login_required
-def edit(request,pk):
+def edit(request, pk):
     tournament = Chess_Tournament.objects.get(pk=pk)
-    games = Chess_Game.objects.filter(tournament=tournament)
-    player = Chess_Player.objects.filter(tournament=tournament)
-    return render(request, 'chesstour/edit.html', {'tournament': tournament,'games':games,'player':player})
+    players = Chess_Player.objects.filter(tournament=tournament)
+
+    if request.method == 'POST':
+        if 'tournament_form' in request.POST:
+            tournament_form = TournamentForm(request.POST,instance=tournament)
+            if tournament_form.is_valid():
+                tournament_form.save()
+                player_form = PlayerForm()
+        elif 'player_form' in request.POST:
+            player_form = PlayerForm(request.POST)
+            if player_form.is_valid():
+                player=player_form.save(commit=False)
+                player.tournament=tournament
+                player.save()
+            tournament_form = TournamentForm(instance=tournament)
+
+        if player_form.errors or tournament_form.errors:
+            print(player_form.errors)
+            print(tournament_form.errors)
+            
+    else:
+        tournament_form = TournamentForm(instance=tournament)
+        player_form = PlayerForm()
+
+    return render(request, 'chesstour/edit.html', {
+        'tournament': tournament,
+        'players': players,
+        'tournament_form': tournament_form,
+        'player_form': player_form
+    })
     
