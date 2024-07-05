982a99228a5e90cea584724a321dc1f13d132047
SilverPung <kristofer.pawel@gmail.com>
2024-07-05 19:20:22 +0200

    created final algorithm

--- chess/chesstour/create_algorythms.py
+++ chess/chesstour/create_algorythms.py
@@ -98,3 +98,47 @@ def determine_white_player(tournament, round):
             game.player1.save()
             game.player2.save()
             game.save()
+
+
+def creating_game_version3(players,games,number_of_rounds):
+
+    sorted_players=list(players.order_by('-rating'))
+    #print(sorted_players)
+    to_create={}
+    
+    repeater=0
+    
+    if len(sorted_players)%2!=0: #if there is odd number of players
+        bye=sorted_players[-1]
+        to_create.update({repeater:(bye,bye)})
+        repeater+=1
+        sorted_players=sorted_players[:-1]
+    j=0
+    #repeater=0/1
+    while len(sorted_players)>0:
+        try:
+            while check_if_in_games(sorted_players[0],sorted_players[j+1],games):
+                j+=1
+        except IndexError: 
+            if len(to_create)==0:
+                game=games[0]
+                Chess_Game.objects.filter(tournament=game.tournament).delete()
+            else:
+                repeater-=1
+                player1,player2=to_create.pop(repeater)
+                sorted_players.insert(0,player1)
+                sorted_players.insert(1,player2)
+                j=1
+                continue
+        
+        player1=sorted_players[0]
+        player2=sorted_players[j+1]
+        sorted_players.remove(player1)
+        sorted_players.remove(player2)
+        to_create.update({repeater:(player1,player2)})
+        repeater+=1
+        j=0 
+    for key,value in to_create.items():
+        player1,player2=value
+        Chess_Game.objects.create(player1=player1,player2=player2,tournament=player1.tournament,round=number_of_rounds+1)
+
--- chess/chesstour/tests.py
+++ chess/chesstour/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .models import Chess_Player, Chess_Game, Chess_Tournament
-from .views import create_game_version1,create_game_version2
+from .create_algorythms import create_game_version1,create_game_version2,creating_game_version3
 from django.contrib.auth.models import User
 from django.test import Client
 from django.urls import reverse
@@ -138,3 +138,99 @@ class TestEditGame(TestCase):
         self.assertEqual(set((games[5].player1, games[5].player2)), set((players[4], players[1])))
         
         #print(Chess_Game.objects.all())
+
+        
+class CreateGameTestVersion3(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.tournament = Chess_Tournament.objects.create(name='Test Tournament', description='Test Description',created_by_id=1)
+        Chess_Player.objects.create(name='Player 1',surname='Player1',tournament=self.tournament, rating=1),#0 
+        Chess_Player.objects.create(name='Player 2',surname='Player2', tournament=self.tournament, rating=0),#1
+        Chess_Player.objects.create(name='Player 3',surname='Player3',tournament=self.tournament, rating=2),#2
+        Chess_Player.objects.create(name='Player 4',surname='Player4',tournament=self.tournament, rating=1),#3
+        Chess_Player.objects.create(name='Player 5',surname='Player5',tournament=self.tournament, rating=1),#4
+        Chess_Player.objects.create(name='Player 6',surname='Player6',tournament=self.tournament, rating=1),#5
+        self.players = Chess_Player.objects.filter(tournament=self.tournament)
+    def test_create_game(self):
+        creating_game_version3( self.players , [], 0)
+        games = Chess_Game.objects.all()
+        self.assertEqual(Chess_Game.objects.count(), 3)
+        self.assertEqual(set((games[0].player1, games[0].player2)), set((self.players[2], self.players[0])))
+        self.assertEqual(set((games[1].player1, games[1].player2)), set((self.players[3], self.players[4])))
+        self.assertEqual(set((games[2].player1, games[2].player2)), set((self.players[5], self.players[1]))
+        )
+    def test_create_game_with_bye(self):
+        players = Chess_Player.objects.filter(id__in=[1, 2, 4, 5, 6 ])
+        creating_game_version3(players, [], 0)
+        games = Chess_Game.objects.all()
+        self.assertEqual(Chess_Game.objects.count(), 3)
+        self.assertEqual(set((games[0].player1, games[0].player2)), set((players[1], players[1])))
+        self.assertEqual(set((games[1].player1, games[1].player2)), set((players[0], players[2])))
+        self.assertEqual(set((games[2].player1, games[2].player2)), set((players[3], players[4])))
+    
+    def test_create_game_with_previous_games(self):
+        
+        creating_game_version3( self.players , [], 0)
+        self.assertEqual(Chess_Game.objects.count(), 3)
+        games=Chess_Game.objects.all()
+        
+        
+        creating_game_version3( self.players , games, 1)
+        self.assertEqual(Chess_Game.objects.count(), 6)
+        games=Chess_Game.objects.all()
+
+        creating_game_version3( self.players , games, 2)
+        self.assertEqual(Chess_Game.objects.count(), 9)
+        games=Chess_Game.objects.all()
+        
+
+        
+        self.assertEqual(set((games[0].player1, games[0].player2)), set((self.players[2], self.players[0])))
+        self.assertEqual(set((games[1].player1, games[1].player2)), set((self.players[3], self.players[4])))
+        self.assertEqual(set((games[2].player1, games[2].player2)), set((self.players[5], self.players[1])))
+
+        self.assertEqual(set((games[3].player1, games[3].player2)), set((self.players[2], self.players[3])))
+        self.assertEqual(set((games[4].player1, games[4].player2)), set((self.players[0], self.players[5])))
+        self.assertEqual(set((games[5].player1, games[5].player2)), set((self.players[4], self.players[1])))
+
+        self.assertEqual(set((games[6].player1, games[6].player2)), set((self.players[2], self.players[4])))
+        self.assertEqual(set((games[7].player1, games[7].player2)), set((self.players[0], self.players[1])))
+        self.assertEqual(set((games[8].player1, games[8].player2)), set((self.players[5], self.players[3])))
+    def test_create_game_with_previous_games_and_changing_score(self):
+        players = Chess_Player.objects.filter(id__in=[1, 2, 3, 4, 5, 6])
+        creating_game_version3( players , [], 0)
+        self.assertEqual(Chess_Game.objects.count(), 3)
+        games=Chess_Game.objects.all()
+        
+        
+        Chess_Player.objects.filter(id=3).update(rating=Chess_Player.objects.get(id=3).rating+1)
+        Chess_Player.objects.filter(id=4).update(rating=Chess_Player.objects.get(id=4).rating+1)
+        Chess_Player.objects.filter(id=6).update(rating=Chess_Player.objects.get(id=6).rating+1)
+        #1,0,3,2,1,2
+        
+        players = Chess_Player.objects.filter(id__in=[1, 2, 3, 4, 5, 6])
+        creating_game_version3( players , games, 1)
+        self.assertEqual(Chess_Game.objects.count(), 6)
+        games=Chess_Game.objects.all()
+
+        Chess_Player.objects.filter(id=4).update(rating=Chess_Player.objects.get(id=4).rating+1)
+        Chess_Player.objects.filter(id=1).update(rating=Chess_Player.objects.get(id=1).rating+1)
+        Chess_Player.objects.filter(id=2).update(rating=Chess_Player.objects.get(id=2).rating+1)
+        #2,1,3,3,1,2
+        
+        players = Chess_Player.objects.filter(id__in=[1, 2, 3, 4, 5, 6])
+        creating_game_version3( players , games, 2)
+        self.assertEqual(Chess_Game.objects.count(), 9)
+        games=Chess_Game.objects.all()
+        
+        self.assertEqual(set((games[0].player1, games[0].player2)), set((players[2], players[0])))
+        self.assertEqual(set((games[1].player1, games[1].player2)), set((players[3], players[4])))
+        self.assertEqual(set((games[2].player1, games[2].player2)), set((players[5], players[1])))
+        
+        self.assertEqual(set((games[3].player1, games[3].player2)), set((players[2], players[3])))
+        self.assertEqual(set((games[4].player1, games[4].player2)), set((players[0], players[5])))
+        self.assertEqual(set((games[5].player1, games[5].player2)), set((players[4], players[1])))
+
+        self.assertEqual(set((games[6].player1, games[6].player2)), set((players[2], players[5])))
+        self.assertEqual(set((games[7].player1, games[7].player2)), set((players[3], players[1])))
+        self.assertEqual(set((games[8].player1, games[8].player2)), set((players[0], players[4])))
